445c996ecd5afce9bd9805958d08d4d756adf9d4
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-11-10 22:28:33 -0600

    added layer switch

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 
 # color generator
-
-
 def color_generator(elevation):
     if elevation < 1000:
         return "green"
@@ -22,20 +20,25 @@ def color_generator(elevation):
 
 # creating map
 map = folium.Map(location=[38, -99], zoom_start=6, tiles="Stamen Terrain")
-
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 # adding marker
 for lt, lg, el in zip(lat, lon, elev):
-    map.add_child(folium.CircleMarker(location=[lt, lg], radius=6, popup=str(
+    fgv.add_child(folium.CircleMarker(location=[lt, lg], radius=6, popup=str(
         el)+" m", fill_color=color_generator(el), color="grey", fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(
     data=open("world.json", "r", encoding="utf-8-sig").read(),
     style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"] <= 10_000_000 
     else "orange" if x["properties"]["POP2005"] <= 20_000_000 else "red"}
     ))
 
 # generating html
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+# adding layer swith: on/off
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
